1aaec0c182dd78f611d30bb921cb06670a899a4b
anudhritik <anukreddy28@gmail.com>
2019-01-21 11:31:50 -0500

    Adding the dockerfiles

--- worker/index.js
+++ worker/index.js
@@ -15,12 +15,12 @@ const redisClient = redis.createClient({
 
 const sub = redisClient.duplicate();
 
-function fib(index){
-    if(index < 2) return 1;
-    return fib(index-1) + fib(index - 2);
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
-    redisClient.hset('values', message, fib(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
